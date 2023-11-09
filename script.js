968f7fea732707369f7d187cd8e64cc572889bd5
clemens <dcleme51@gmail.com>
2023-11-09 23:51:39 +0000

    pseducode and attempting firt 2 lines of code

---
+++ script.js
@@ -0,0 +1,19 @@
+//Test out console to make sure its linked to HTML file.
+// console.log("Drums"); - works
+
+// When a kbd letter is seletced the js should play an audio.
+//create an array of the letters that we want to target when selcted.
+var drumKey = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+
+//when one of the keys in the array is pressed an audio will be played. You can use an Event onkeydown to perform the action.
+onkeydown(drumKey) = document.addEventListener{
+audio
+}
+
+//create an event audio when a key in the array is selected.
+
+
+
+//
+
+// At the same time when key entered and playing an audio the letter in corrspondance should pop up a little with a boarder orenge.
